2a19a206a0e21345b9905bb977f93b92956fd0cc
SonuTheNecro <156054782+SonuTheNecro@users.noreply.github.com>
2024-03-11 18:07:50 -0500

    fixed logic
    added env support, changed frame splicing logic, better download solution

--- main.py
+++ main.py
@@ -1,54 +1,53 @@
 import tweepy,time,gdown,sys
 import numpy as np
 import cv2 as cv
-
+import subprocess
+import os
+from dotenv import load_dotenv
 # keys
-api_key = "XXX"
-api_key_secret = "XXX"
-bearer_token = "XXX"
-access_token = "XXX"
-access_token_secret = "XXX"
-client_id = "XXX"
-client_secret = "XXX"
 
+api_key = os.getenv("api_key")
+api_key_secret = os.getenv("api_key_secret")
+bearer_token = os.getenv("bearer_token")
+access_token = os.getenv("access_token")
+access_token_secret = os.getenv("access_token_secret")
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+""""
 # Tweepy Setup
 client = tweepy.Client(bearer_token, api_key, api_key_secret, access_token, access_token_secret)
 auth = tweepy.OAuthHandler(api_key, api_key_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
+"""
 
 #Set default
 season = 1
 total_episode = 1
 episode = 1
-#Reading urls from text file
-url_list = open(R"all_urls.txt","r")
-Lines = url_list.readlines()
 
-#Pulling Video from google drive
-for Line in Lines:
-    google_drive_url = Line
-    output_file = "current_video"
-    id = google_drive_url.split('/')[-2]
-    gdown.download(f"https://drive.google.com/uc?id={id}", output_file)
+while(True):
+    #Runs animdl to download a single episode
+    subprocess.run(["animdl download 'Yu-Gi-Oh! -r {total_episode} -d C:/Users/Bhaar/Documents/Code/ygoframes/current_video"])
 
-    #Downloads all the frames of a video (1 fps video)
-    capture = cv.VideoCapture("C:/Users/Bhaar/Documents/Code/ygoframes/current_video.mp4")
+    #Converts the video into frames
+    capture = cv.VideoCapture('{:02d}.mp4'.format(total_episode))
     count = 1
-    while(True):
-        success, frame = capture.read()
+    success, frame = capture.read()
+    while(success):
         if success and count % 24 == 0: 
             cv.imwrite(f"C:/Users/Bhaar/Documents/Code/ygoframes/current_video_frames/{count / 24}.jpg", frame)
-        else:
-            break
+        success, frame = capture.read()
         count += 1
     capture.release()
 
+    os.remove('{:02d}.mp4'.format(total_episode))
     #setup episode for caption
     count = count / 24
     count2 = 1
     while count2 < count:
         caption_text = "Yu-Gi-Oh! Duel Monsters - Season {season} Episode {episode} - Frame {count2} of {count}"
-        client.create_tweet(text = caption_text, media="C:/Users/Bhaar/Documents/Code/ygoframes/current_video_frames/{count2}.jpg")
+        #client.create_tweet(text = caption_text, media="C:/Users/Bhaar/Documents/Code/ygoframes/current_video_frames/{count2}.jpg")
+        print(caption_text)
         count2 +=1
         time.sleep(1800)
     
